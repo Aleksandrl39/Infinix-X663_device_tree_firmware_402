742b664fbc034b56aabd81d41334c5ba469d5ac2
Aleksandrl39 <40720446+Aleksandrl39@users.noreply.github.com>
2025-11-08 21:11:01 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/infinix/Infinix-X663
+LOCAL_PATH := device/infinix/Infinix_X663
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
